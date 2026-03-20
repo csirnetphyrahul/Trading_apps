905a8bbb34d69bf2467711fa15fcf8b633b293d1
csirnetphyrahul <csirnetphy@gmail.com>
2026-03-20 16:16:07 +0530

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# ===== INPUT STRUCTURE =====
+class TradeRequest(BaseModel):
+    symbol: str
+    qty: int
+    side: str   # BUY / SELL
+    sl: float
+
+# ===== HEALTH CHECK =====
 @app.get("/")
 def home():
     return {"status": "running"}
 
+# ===== TRADE API =====
 @app.post("/trade")
-def trade():
-    return {"message": "API working"}
+def trade(data: TradeRequest):
+
+    print("TRADE RECEIVED:", data)
+
+    return {
+        "status": "executed",
+        "symbol": data.symbol,
+        "side": data.side,
+        "qty": data.qty,
+        "sl": data.sl
+    }
